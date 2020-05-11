529e6699191d198b108bf9ce2cc74533afe58846
mjolnir-asgard <49009913+mjolnir-asgard@users.noreply.github.com>
2020-05-11 11:04:12 -0500

    project init...

---
+++ src/main/java/gov/nasa/NasaDataApplication.java
@@ -0,0 +1,13 @@
+package gov.nasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasaDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasaDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gov/nasa/NasaDataApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.nasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasaDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
